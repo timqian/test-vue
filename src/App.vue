63c81e5e1752c5de82333a243e1cb3ca996ff3e6
timqian <timqian92@qq.com>
2016-01-15 15:44:43 +0800

    favicon

--- src/App.vue
+++ src/App.vue
@@ -5,17 +5,17 @@
 </template>
 
 <script>
-export default {
-  data () {
-    return {
-      // note: changing this line won't causes changes
-      // with hot-reload because the reloaded component
-      // preserves its current state and we are modifying
-      // its initial state.
-      msg: 'Hello Vue!'
+  export default {
+    data () {
+      return {
+        // note: changing this line won't causes changes
+        // with hot-reload because the reloaded component
+        // preserves its current state and we are modifying
+        // its initial state.
+        msg: 'Hello Vue!'
+      }
     }
   }
-}
 </script>
 
 <style>
